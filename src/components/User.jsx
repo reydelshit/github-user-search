7d260e54f0af88c859247462ac88eeeb4425b631
Reydel Ocon <reydel321@gmail.com>
2022-02-24 13:11:52 +0800

    test

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -17,7 +17,7 @@ const User = ({userData}) => {
       {userData.map((wow, index) => 
         <div key={index}>
           <div className='flex w-full items-center justify-around mb-5'>
-              <img className='w-3/12 md:w-28 h-1/4 md:h-28 object-fill rounded-full mr-3 md:mx-2' src={wow.avatar_url} alt="profile" />
+              <img className='w-3/12 h-1/4 md:w-28 md:h-28 object-fill rounded-full mr-3 md:mx-2' src={wow.avatar_url} alt="profile" />
           <div className='flex flex-col md:flex-row w-full items-start p-1 md:p-5'>
             <div className='flex flex-col items-start w-full mb-1'>
               <h2 className='text-cyan-300 font-bold text-xl md:text-2xl dark:text-secondaryColor'>{wow.name}</h2>
